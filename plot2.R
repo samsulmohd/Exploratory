6e12666b3ef9eb09c5a3b3b322e7353d2e2d7d15
samsulmohd <root.cracker@gmail.com>
2015-08-10 05:12:47 +0800

    assignment 1

---
+++ plot2.R
@@ -0,0 +1,21 @@
+setwd("E:/R/Git/Exploratory_Data_Analysis")
+
+## Get all data
+data_all <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                     nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_all$Date <- as.Date(data_all$Date, format="%d/%m/%Y")
+
+## Target data
+data <- subset(data_all, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Convert date
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+## Save to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
